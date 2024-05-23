5bf607eb32a36542483b5e293582bad048725502
dheerendra ghosh <dheerendraghoshghosh6@gmail.com>
2024-05-23 17:42:05 +0530

    implement eddit button

--- functions.py
+++ functions.py
@@ -1,10 +1,13 @@
 def get_todos(filepath='todos.txt'):
-    """read the text fiel and return the todos list with smile"""
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-    return todos
+    """Read the text file and return the todos list"""
+    try:
+        with open(filepath, 'r') as file:
+            todos = file.readlines()
+    except FileNotFoundError:
+        todos = []
+    return [todo.strip() + '\n' for todo in todos]
 
-
-def write_todos(todos_arg,file_path='todos.txt'):
-    with open('todos.txt', 'w') as file:
+def write_todos(todos, filepath='todos.txt'):
+    """Write the list of todos to the text file"""
+    with open(filepath, 'w') as file:
         file.writelines(todos)
--- gui.py
+++ gui.py
@@ -1,13 +1,60 @@
-import functions
 import PySimpleGUI as sg
+import functions
 
-label=sg.Text('hello welcome to my todo  web app')
-input_box=sg.Input('enter todo')
-add_button=sg.Button('ADD')
-edit_button=sg.Button('EDIT')
-remove_button=sg.Button('REMOVE')
+# Define the UI elements
+welcome_message = sg.Text('Welcome to My To-Do List Manager')
+input_box = sg.InputText(tooltip='Enter your to-do item here', key='todo')
+add_button = sg.Button('Add', tooltip='Add a new to-do item')
 
-layout=[[label],[input_box],[add_button,edit_button,remove_button]]
-window=sg.Window('this is my todo app',layout)
-window.read()
+list_box=sg.Listbox(values=functions.get_todos(),size=[40,10] ,enable_events=True,key='todos')
+
+
+
+
+
+edit_button = sg.Button('Edit', tooltip='Edit the selected to-do item')
+remove_button = sg.Button('Remove', tooltip='Remove the selected to-do item')
+
+# Define the layout
+layout = [
+    [welcome_message],[list_box],
+    [input_box],
+    [add_button, edit_button, remove_button]
+]
+
+# Create the window with the specified title and layout
+window = sg.Window('To-Do List Manager', layout ,font=('Helvetica', 20))
+
+# Start the event loop
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Add':
+        todos = functions.get_todos()
+        new_todo = values['todo'].strip() + '\n'
+        todos.append(new_todo)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+    elif event == 'Edit':
+        edit_todo=values['todos'][0]
+        new_todo=values['todo']
+
+        todos=functions.get_todos()
+        index=todos.index(edit_todo)
+        todos[index]=new_todo
+
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+
+    elif event=='todos':
+        window['todo'].update(value='todos')
+    elif event == 'Remove':
+        # Add your remove logic here
+        pass
+
+
+
 window.close()
