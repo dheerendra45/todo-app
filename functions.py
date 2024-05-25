f9b27795cfb802880da189aaa2c34030f55093e4
dheerendra ghosh <dheerendraghoshghosh6@gmail.com>
2024-05-25 19:08:38 +0530

    add clock and implement edit and complete button

--- functions.py
+++ functions.py
@@ -1,10 +1,16 @@
-def get_todos(filepath='todos.txt'):
-    """read the text fiel and return the todos list with smile"""
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-    return todos
 
+def get_todos():
+    try:
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        return todos
+    except Exception as e:
+        sg.popup(f"An error occurred while reading the file: {e}")
+        return []
 
-def write_todos(todos_arg,file_path='todos.txt'):
-    with open('todos.txt', 'w') as file:
-        file.writelines(todos)
+def write_todos(todos):
+    try:
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
+    except Exception as e:
+        sg.popup(f"An error occurred while writing to the file: {e}")
